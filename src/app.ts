2513a554fa01dbfa1d0cb5edfca5f6d9ba44f867
asamarsal <ASAMARSAL@gmail.com>
2025-08-01 12:32:42 +0700

    Deploy

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://circle-app-chba.vercel.app/"],
     credentials: true,
     methods: ["GET", "POST"]
   }
--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,7 +1,7 @@
 import cors from "cors";
 
 export const corsMiddleware = cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://circle-app-chba.vercel.app"],
     credentials: true,
     methods: ['GET', 'PUT', 'POST', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
